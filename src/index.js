4cb280a39ebd244afbbfb6e99fd5b54b312a0bb0
jakubmanczak <j4kubmanczak@gmail.com>
2021-04-25 13:02:51 +0200

    :sparkles: updated webPrefs

--- src/index.js
+++ src/index.js
@@ -14,6 +14,10 @@ const createWindow = () => {
     height: 315,
     frame: false,
     resizable: false,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
   });
 
   // and load the index.html of the app.
